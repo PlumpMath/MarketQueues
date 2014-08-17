3f702a4e8c42426ed1d09d259022b701f3a878f5
artem pecheny <pecheny@gmail.com>
2014-08-17 15:06:01 +0400

    Checkout counters view refactoring

--- src/main/java/shoo/competentum/client/view/CountersView.java
+++ src/main/java/shoo/competentum/client/view/CountersView.java
@@ -39,54 +39,63 @@ public class CountersView extends Composite {
 		initWidget(container);
 		for (CheckoutCounter counter : counters) {
 			int statesCount = 0;
+			int performance = counter.getPerformance();
+			int rowHeight = performance * (ITEM_HEIGHT + SPACING_HEIGHT);
 			DrawingArea counterCanvas = new DrawingArea((counter.getHistory().size() + 1) * (UNIT_WIDTH + SPACING_WIDTH), 100);
 
-			int rowHeight = counter.getPerformance() * (ITEM_HEIGHT + SPACING_HEIGHT);
+
 			drawGrid(counterCanvas, 5 * (UNIT_WIDTH + SPACING_WIDTH), rowHeight, "#eeeeee", 1.0, 1, 0);
-			Rectangle rectangle = new Rectangle(0, 0, counterCanvas.getWidth(), rowHeight);
-			rectangle.setFillColor("#e0e0e0");
-			rectangle.setStrokeWidth(0);
-			counterCanvas.add(rectangle);
+			drawRect(counterCanvas, 0, 0, counterCanvas.getWidth(), rowHeight, "#e0e0e0");
+
 
 			for (State state : counter.getHistory()) {
 				statesCount++;
-				int customersCount = 0;
-				int lastY = rowHeight;
-				for (Customer customer : state.getQueue()) {
-					int unusedSlotsForNextStep = customer.getItemsInCart() % counter.getPerformance() == 0 ? 0 : counter.getPerformance() - customer.getItemsInCart() % counter.getPerformance();
-					int customerHeight = customer.getItemsInCart() * (ITEM_HEIGHT + SPACING_HEIGHT);
-					if (unusedSlotsForNextStep == 0) {
-						customerHeight -= SPACING_HEIGHT;
-					}
-
-
-					rectangle = new Rectangle(statesCount * (UNIT_WIDTH + SPACING_WIDTH), lastY, UNIT_WIDTH, customerHeight);
-					rectangle.setFillColor(colors.get(customer.getKind()));
-					rectangle.setStrokeWidth(0);
-					counterCanvas.add(rectangle);
-					customersCount++;
-					if (customersCount < state.getQueue().size()) { // сохранить значение lastY для последнего покупателя,
-						//                                             чтобы можно было воспользоваться им для отрисовки маркера "новый"
-						lastY += customerHeight + (unusedSlotsForNextStep) * (ITEM_HEIGHT + SPACING_HEIGHT);
-						if (unusedSlotsForNextStep == 0) {
-							lastY += SPACING_HEIGHT;
-						}
-					}
-
-				}
-				if (state.getCustomerAdded()) {
-					Circle circle = new Circle(statesCount * (UNIT_WIDTH + SPACING_WIDTH) + UNIT_WIDTH / 2, lastY + UNIT_WIDTH / 2, UNIT_WIDTH / 2);
-					circle.setFillColor("#ffae00");
-					circle.setStrokeWidth(0);
-
-					counterCanvas.add(circle);
-				}
+				int x0 = statesCount * (UNIT_WIDTH + SPACING_WIDTH);
+				renderState(counterCanvas, state, performance, x0, rowHeight);
 			}
+
 			drawGrid(counterCanvas, counterCanvas.getWidth(), (ITEM_HEIGHT + SPACING_HEIGHT), "#ffffff", 0.5, SPACING_HEIGHT, -rowHeight + SPACING_HEIGHT / 2);
-			container.add(new HTML("<h2>Касса с производительностью " + counter.getPerformance() + "</h2>"));
+			container.add(new HTML("<h2>Касса с производительностью " + performance + "</h2>"));
 			container.add(counterCanvas);
 		}
 	}
+
+	private void renderState(DrawingArea counterCanvas, State state, int performance, int x0, int y0) {
+		int customersCount = 0;
+		int lastY = y0;
+
+		for (Customer customer : state.getQueue()) {
+			int unusedSlotsForNextStep = customer.getItemsInCart() % performance == 0 ? 0 : performance - customer.getItemsInCart() % performance;
+			int customerHeight = customer.getItemsInCart() * (ITEM_HEIGHT + SPACING_HEIGHT);
+			if (unusedSlotsForNextStep == 0) {
+				customerHeight -= SPACING_HEIGHT;
+			}
+			drawRect(counterCanvas, lastY, x0, UNIT_WIDTH, customerHeight, colors.get(customer.getKind()));
+			customersCount++;
+			if (customersCount < state.getQueue().size()) { // сохранить значение lastY для последнего покупателя,
+				//                                             чтобы можно было воспользоваться им для отрисовки маркера "новый"
+				lastY += customerHeight + (unusedSlotsForNextStep) * (ITEM_HEIGHT + SPACING_HEIGHT);
+				if (unusedSlotsForNextStep == 0) {
+					lastY += SPACING_HEIGHT;
+				}
+			}
+		}
+		if (state.getCustomerAdded()) {
+			Circle circle = new Circle(x0 + UNIT_WIDTH / 2, lastY + UNIT_WIDTH / 3, UNIT_WIDTH / 3);
+			circle.setFillColor("#ffae00");
+			circle.setStrokeWidth(0);
+			counterCanvas.add(circle);
+		}
+	}
+
+	private void drawRect(DrawingArea counterCanvas, int y, int x, int width, int height, String color) {
+		Rectangle rectangle;
+		rectangle = new Rectangle(x, y, width, height);
+		rectangle.setFillColor(color);
+		rectangle.setStrokeWidth(0);
+		counterCanvas.add(rectangle);
+	}
+
 
 	private void drawGrid(DrawingArea canvas, int gWidth, int gHeight, String color, Double opacity, int thickness, int yoffset) {
 		for (int pos = gWidth; pos < canvas.getWidth(); pos += gWidth) {
